618b582d2142fdf1f58506597d069260387723d7
cibin mathew <ikonland@gmail.com>
2017-10-21 15:43:01 +0530

    home directory

---
+++ delete.sh
@@ -0,0 +1,5 @@
+echo "$@">delete.txt
+source ~/.bashrc
+
+cat "$Universal_home/Downloads/all_files.db"|fzy -e=control
+# echo "delete commanddd"
---
+++ my-scripts/default_run.sh
@@ -0,0 +1,9 @@
+echo "$1"
+
+file=$(~/my-scripts/convert_path_to_windows.sh "$*")
+
+echo "$file"
+# open explorer.bat "$file"
+
+# cygwin works for below
+explorer.exe  "$file"
